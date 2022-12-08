8372bf62d18e3e40bd1cb645126da0c609fe3ea5
Eduardoooxd <ecouto93@gmail.com>
2022-12-08 16:37:54 +0000

    feat: adding remaining components for initial page

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,15 +1,22 @@
 import { FunctionComponent } from "react";
+import Footer from "../Footer";
 import Navbar from "../Navbar";
 
 interface LayoutProps {
 	children?: React.ReactNode;
 }
 
+const GoToTopButton: FunctionComponent = () => {
+	return <button className="goTop fas fa-arrow-up"></button>;
+};
+
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
 	return (
 		<div className="">
 			<Navbar />
 			{children}
+
+			<Footer />
 		</div>
 	);
 };
--- components/Products/index.tsx
+++ components/Products/index.tsx
@@ -20,15 +20,72 @@ const Products: FunctionComponent<ProductsProps> = () => {
 		<section>
 			<div className="inner-width">
 				<h2 className="section-title">OS NOSSOS PRODUTOS</h2>
-				<h3 className="produtos-title">SOLUÇÕES EMPRESARIAIS</h3>
 
-				<div className="grid-container grid-uso-industrial">
-					<div className="responsive">
-						<div className="gallery">
-							<div className="des">Água KMZERO</div>
-							<a href="kmzero" className="work">
-								{/**<Image src="images/produtos/kmzero-transparente.png" />*/}
-							</a>
+				<div>
+					<h3 className="produtos-title">SOLUÇÕES EMPRESARIAIS</h3>
+
+					<div className="grid-container grid-uso-industrial">
+						<div className="responsive">
+							<div className="gallery">
+								<div className="des">Água KMZERO</div>
+								<a href="kmzero" className="work">
+									{/**<Image src="images/produtos/kmzero-transparente.png" />*/}
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div>
+					<h1 className="produtos-title">SOLUÇÕES DOMICILIARES</h1>
+
+					<div className="grid-container">
+						<div className="responsive">
+							<div className="gallery">
+								<div className="des">Hydron</div>
+								<a href="hydron" className="work">
+									<img
+										src="images/produtos/hydron.png"
+										alt="aquapharma hydron"
+									/>
+								</a>
+							</div>
+						</div>
+
+						<div className="responsive">
+							<div className="gallery">
+								<div className="des">Purificadores</div>
+								<a href="purificadoragua" className="work">
+									<img
+										src="images/produtos/advance2.png"
+										alt="aquapharma purificador agua"
+									/>
+								</a>
+							</div>
+						</div>
+
+						<div className="responsive">
+							<div className="gallery">
+								<div className="des">Magnetoterapia</div>
+								<a href="magnetoterapia" className="work">
+									<img
+										src="images/produtos/magnetoSecundary.png"
+										alt="aquapharma magneto"
+									/>
+								</a>
+							</div>
+						</div>
+
+						<div className="responsive">
+							<div className="gallery">
+								<div className="des">Laundry Pro</div>
+								<a href="laundry" className="work">
+									<img
+										src="images/produtos/Laundry-Pro-Vipur-2.png"
+										alt="aquapharma Laundry-Pro"
+									/>
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
