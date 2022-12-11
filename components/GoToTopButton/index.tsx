8ce400a53ba445b6ea3ed774b018113b9f178a3f
Eduardoooxd <ecouto93@gmail.com>
2022-12-11 17:47:58 +0000

    feat: improving logic of go to top button

---
+++ components/GoToTopButton/index.tsx
@@ -0,0 +1,34 @@
+import { FunctionComponent, useEffect, useState } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+const GoToTopButton: FunctionComponent = () => {
+	const OFFSET_TO_VISIBLE = 20;
+	const [visible, setVisible] = useState(false);
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	});
+	const scrollToTop = () => {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	};
+
+	const handleScroll = () => {
+		setVisible(window.scrollY > OFFSET_TO_VISIBLE);
+	};
+
+	return (
+		<button
+			onClick={scrollToTop}
+			className={`${
+				visible ? "block" : "invisible"
+			} group fixed bg-aquapharma-blue bottom-10 right-10 aspect-square w-10 rounded-lg flex justify-center items-center `}>
+			<FaArrowUp className="group-hover:scale-125 transition duration-300 text-white w-full " />
+		</button>
+	);
+};
+
+export default GoToTopButton;
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,26 +1,11 @@
 import { FunctionComponent } from "react";
-import { FaArrowUp } from "react-icons/fa";
 import Footer from "../Footer";
+import GoToTopButton from "../GoToTopButton";
 import Navbar from "../Navbar";
 
 interface LayoutProps {
 	children?: React.ReactNode;
 }
-
-const GoToTopButton: FunctionComponent = () => {
-	const scrollToTop = () => {
-		window.scrollTo({ top: 0, behavior: "smooth" });
-	};
-
-	return (
-		<button
-			hidden
-			onClick={scrollToTop}
-			className="group fixed bg-aquapharma-blue bottom-10 right-10 aspect-square w-10 rounded-lg flex justify-center items-center ">
-			<FaArrowUp className="group-hover:scale-125 transition duration-300 text-white w-full " />
-		</button>
-	);
-};
 
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
 	return (
